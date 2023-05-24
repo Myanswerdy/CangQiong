a50daaaf3805dbd1da17536a9a52cf0c5b9603ce
kwydy <2273024587@qq.com>
2023-05-24 11:40:57 +0800

    地址簿功能，用户下单功能

--- sky-common/src/main/java/com/sky/utils/JwtUtil.java
+++ sky-common/src/main/java/com/sky/utils/JwtUtil.java
@@ -27,6 +27,7 @@ public class JwtUtil {
         long expMillis = System.currentTimeMillis() + ttlMillis;
         Date exp = new Date(expMillis);
 
+
         // 设置jwt的body
         JwtBuilder builder = Jwts.builder()
                 // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
